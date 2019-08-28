a06e43fa6b1e696d73ab7e0bf4f97ed21d545fcc
Conner <peyaconn@iu.edu>
2019-08-28 18:59:50 -0400

    Completes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Peyton Conner')
+print('My favorite game is Pokemon!')
+print('My concerns for this class are mostly around the fact that I have never interacted with computer code at all ' )
+print('I am excited to actually create something worth being proud of in the terms of programming')
+print('My stackoverflow user number is 11991581')
+print('The link to my github profile is https://github.com/peyloading')
